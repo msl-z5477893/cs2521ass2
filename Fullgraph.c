d963ba6270c318406758060d2e7b2ad5c6721b3b
Michael Stephen Lape <z5477893@ad.unsw.edu.au>
2024-07-31 22:02:44 +1000

    continued djikstra algorithm for task 3

--- Fullgraph.c
+++ Fullgraph.c
@@ -6,8 +6,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "Bookmark.h"
+#include "PqNeighbours.h"
+#include "StackInt.h"
+
 #define INF 2147483647
 
+struct weightedDestEdge {
+	int dest;
+	int edgeCost;
+};
+
+struct weightedDestEdge *getEdgesOrdered(int *edgeArray, int arrCount,
+                                         int *newArrCountOut);
 static bool directionalAccess(int securityFrom, int securityTo);
 
 Fullgraph FullgraphGenerate(int numComps, int numConns, struct computer comps[],
@@ -43,9 +54,11 @@ Fullgraph FullgraphGenerate(int numComps, int numConns, struct computer comps[],
 		int secB = comps[idCompB].securityLevel;
 		if (directionalAccess(secA, secB)) {
 			graph->edges[idCompA][idCompB] = connCost;
+			graph->edgeCount[idCompA]++;
 		}
 		if (directionalAccess(secB, secA)) {
 			graph->edges[idCompB][idCompA] = connCost;
+			graph->edgeCount[idCompB]++;
 		}
 	}
 
@@ -66,6 +79,7 @@ void FullgraphFree(Fullgraph graph) {
 // TODO: incomplete
 struct djikstraData *FullgraphDjikstra(Fullgraph g, int vertexSrc) {
 	struct djikstraData *data = malloc(sizeof(*data));
+	// bool *visited = malloc(sizeof(*visited) * g->vertices);
 	data->vertices = g->vertices;
 	data->visits = 0;
 	data->distance = malloc(sizeof(*data->distance) * data->vertices);
@@ -74,14 +88,68 @@ struct djikstraData *FullgraphDjikstra(Fullgraph g, int vertexSrc) {
 		data->predecessor[ix] = -1;
 		data->distance[ix] = INF;
 	}
-
 	data->distance[vertexSrc] = 0;
 
+	Bookmark visited = BookmarkInit(data->vertices);
+	PqNeighbours pqueue = PqNeighboursNew();
+	// StackInt stack = StackIntNew();
+
+	// StackIntPush(stack, vertexSrc);
+	PqNeighboursPush(pqueue, vertexSrc, data->distance[vertexSrc]);
+	while (pqueue->count > 0) {
+		// int currVtx = PqNeighbours(stack);
+		struct neighbourPath currNode = PqNeighboursPop(pqueue);
+		int currVtx = currNode.vertexTo;
+		int currDist = currNode.cost;
+		BookmarkMark(visited, currVtx);
+		int edgesLiveNum = 0;
+		struct weightedDestEdge *liveEdges =
+		    getEdgesOrdered(g->edges[currVtx], g->vertices, &edgesLiveNum);
+		/* FIXME: too much nesting
+                 * this error may cause major marking penalties
+                 */
+		for (int edgesIx = 0; edgesIx < g->edgeCount[currVtx];) {
+			if (g->edges[currVtx][edgesIx] > 0) {
+				int newDist = g->edges[currVtx][edgesIx] + currDist;
+				if (newDist < data->distance[edgesIx]) {
+					data->distance[edgesIx] = newDist;
+					data->predecessor[edgesIx] = currVtx;
+					PqNeighboursPush(pqueue, edgesIx, newDist);
+				}
+			}
+		}
+	}
+
 	printf("WARNING: used function FullgraphDjikstra() in Fullgraph.c is "
-	       "incomplete.\n");
+	       "potentially buggy.\n");
+
+	// TODO: write debugging code here
+
+	PqNeighboursFree(pqueue);
+	BookmarkFree(visited);
 	return data;
 }
 
 static bool directionalAccess(int securityFrom, int securityTo) {
 	return !(securityTo > securityFrom + 1);
+}
+
+struct weightedDestEdge *getEdgesOrdered(int *edgeArray, int arrCount,
+                                         int *newArrCountOut) {
+	PqNeighbours pq = PqNeighboursNew();
+	*newArrCountOut = 0;
+	for (int ix = 0; ix < arrCount; ix++) {
+		if (edgeArray[ix] != 0) {
+			PqNeighboursPush(pq, ix, edgeArray[ix]);
+			(*newArrCountOut)++;
+		}
+	}
+	struct weightedDestEdge *edges = malloc(sizeof(*edges) * (*newArrCountOut));
+	for (int ix = 0; ix < *newArrCountOut; ix++) {
+		struct neighbourPath path = PqNeighboursPop(pq);
+		edges[ix].dest = path.vertexTo;
+		edges[ix].edgeCost = path.cost;
+	}
+	PqNeighboursFree(pq);
+	return edges;
 }
